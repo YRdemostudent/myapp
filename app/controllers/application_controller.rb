dd60439af11e1e32339981bcdbc2252c722ccbfa
YanetRoman <117314595+yanettechprep@users.noreply.github.com>
2023-05-15 20:49:58 +0000

    added homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
 
   def index
-    render("/", {:template =>"homepage/index.html.erb"})
+
+    render({:template =>"homepage/index.html.erb"})
 end
+end 
